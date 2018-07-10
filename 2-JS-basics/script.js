76fecb4697f10f91457101ef01339cfddf7368c9
mrmufo <rykala.wojciech@gmail.com>
2018-07-10 17:36:45 +0100

    Some basic operators

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -22,6 +22,7 @@ console.log(job);
 /**************************
 * Variable mutation and type corection
 */
+/*
 
 var firstName = 'John';
 var age = 28;
@@ -43,3 +44,31 @@ alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + i
 
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
+*/
+
+/**************************
+* Basic operators
+*/
+var year, yearJohn, yearMark;
+year = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+//Math operations
+yearJohn = year - 28;
+
+console.log(yearJohn);
+console.log(year + 2);
+console.log(year * 2);
+console.log(year / 10);
+
+//Logical operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
