7dc1d4297f270168aa8a26c00b98ba37e37145f8
mrmufo <rykala.wojciech@gmail.com>
2018-07-18 18:56:27 +0100

    Added arrays

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -314,6 +314,7 @@ yearsUntilRetirement(1969, 'Jane');
 * Function Statements and Expressions
 */
 
+/*
 // Function declaration
 // function WhatDoYouDo(job, firstName) {}
 
@@ -334,6 +335,52 @@ var whatDoYouDo = function(job, firstName) {
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('designer', 'Jane'));
 console.log(whatDoYouDo('retired', 'Mark'));
+*/
+
+/**************************
+* Arrays
+*/
+
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+//Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
