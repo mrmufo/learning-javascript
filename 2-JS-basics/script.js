f5edd03969cf37eac7b94ab1e47b04d84e10135d
mrmufo <rykala.wojciech@gmail.com>
2018-07-25 19:04:25 +0100

    Added loops examples

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -453,6 +453,7 @@ console.log(john);
 * Challenge 4
 */
 
+/*
 var mark = {
     firstName: 'Mark',
     weight: 75,
@@ -481,19 +482,46 @@ if (mark.BMI > john.BMI) {
 } else {
     console.log('Their BMI\'s are the same: ' + john.BMI + ' = ' + mark.BMI);
 }
+*/
 
+/**************************
+* Loops and iteration
+*/
 
+/*
+for (var i = 0; i < 10; i += 2) {
+    console.log(i);
+}
 
+var john= ['John', 'Smith', 1990, 'designer', false];
 
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+}
 
+var i = 0;
+while (i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+// continue and break statements
+var john= ['John', 'Smith', 1990, 'designer', false];
 
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') continue;
+    console.log(john[i]);
+}
 
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string') break;
+    console.log(john[i]);
+}
 
-
-
-
-
-
+// Looping backwards
+for (var i = john.length-1; i >= 0; i--) {
+    console.log(john[i]);
+}
 
 
 
